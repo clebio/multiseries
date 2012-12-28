f311c3bfc8a3bd7352d69c828bc0a528853268f7
Caleb Hyde <caleb.hyde@gmail.com>
2012-12-28 00:49:56 +0000

    Clean up logic; add License & Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+#Multi-series Line chart using Rickshaw
+This is a reimplementation of the [D3 line chart example][d3msl] that uses the [Rickshaw time-series toolkit][rick].
+
+[d3msl]: http://bl.ocks.org/3884955
+[rick]: http://code.shutterstock.com/rickshaw/
--- multiseries.js
+++ multiseries.js
@@ -1,12 +1,12 @@
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
+width = 960 - margin.left - margin.right,
+height = 500 - margin.top - margin.bottom;
+
 d3.tsv("data.tsv", function (d) {
     var series = d3.keys(d[0]).filter(function(key) { return key !== "date"; });
     var colors = d3.scale.category10();
     colors.domain(series);
 
-    var margin = {top: 0, right: 0, bottom: 0, left: 0},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-    
     var fmt = d3.time.format("%Y%m%d");
     var x0 = fmt.parse(d[0]['date']);
     d.forEach(function(o) { o.date = fmt.parse(o.date) - x0; });
@@ -20,14 +20,19 @@ d3.tsv("data.tsv", function (d) {
 	    })
 	};
     });
-    
+
+    multigraph(dataSets);
+});
+
+var multigraph = function(sets) {
     var graph = new Rickshaw.Graph( {
 	element: document.querySelector("#chart"), 
 	renderer: 'line',
 	width: width, 
 	height: height, 
-	series: dataSets
+	series: sets
     });
 
     graph.render();
-});
+
+};
